43b34c216287be2eb3432dc3a50ee33f36842e9c
Ivelt Julien <ijulien@iveltj.com>
2020-05-16 16:33:31 -0400

    modif module

--- cnnpam.py
+++ cnnpam.py
@@ -8,8 +8,9 @@ Created on Sat May  9 10:45:22 2020
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
-from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Flatten
+
 
 
 # Initialising the CNN
