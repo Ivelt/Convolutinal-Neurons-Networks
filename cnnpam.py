2682eefb03c21cd2a9b6d68276495b4d6b9380a5
Ivelt Julien <ijulien@iveltj.com>
2020-05-16 15:50:01 -0400

    project init

---
+++ cnnpam.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  9 10:45:22 2020
+
+@author: ivelt
+"""
+
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
